932f0ee4c30d654280c67dffb31649f518c3e048
Akarsh <akarshsingh81@gmail.com>
2023-04-11 04:53:17 +0530

    added design points

--- bank application/src/Main.java
+++ bank application/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello this is my application!");
+        //we ll be creating a bank app today
+        // features to be added -
+        // 1 - create bank account
+        // 2 - add money
+        // 3 - check balance
+        // 4 - withdraw balance
+
     }
 }
