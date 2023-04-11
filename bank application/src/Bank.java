82693a1d672a96b7ce28c2bd3820bdabc9647e90
Akarsh <akarshsingh81@gmail.com>
2023-04-11 06:04:54 +0530

    made an interface Bank

---
+++ bank application/src/Bank.java
@@ -0,0 +1,8 @@
+public interface Bank {
+
+    int checkBalance();
+    boolean addMoney(int money);
+    boolean withdrawMoney(int money);
+    int getROI();
+    int totalInterest(int loanAmount,int timeInYears);
+}
